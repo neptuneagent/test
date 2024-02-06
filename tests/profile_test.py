64277ba438ba9e1f89f2f0f19406887b2f15ddc4
neptuneagent <133614371+neptuneagent@users.noreply.github.com>
2024-02-06 12:06:37 +0700

    Update profile_test.py

--- tests/profile_test.py
+++ tests/profile_test.py
@@ -6,9 +6,9 @@ class ProfilePictureUploadTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        option = webdriver.FirefoxOptions()
+        option = webdriver.EdgeOptions()
         option.add_argument('--headless')
-        cls.browser = webdriver.Firefox(options=option)
+        cls.browser = webdriver.Edge(options=option)
         try:
             cls.url = os.environ['URL']
         except:
